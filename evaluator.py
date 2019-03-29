614402a38c985f00e8d118bbd69eb3b888b68182
maxrossi91 <maxrossi91@gmail.com>
2019-03-29 16:47:21 +0100

    Added evaluator

---
+++ evaluator.py
@@ -0,0 +1,279 @@
+import turingarena as ta
+import networkx as nx
+from networkx.algorithms import bipartite
+
+DEBUG = False
+
+# Goals:
+# time limit = 0.2s
+# - lineare     O(M + N)    N <= 2000, M <= 2000
+# - quadratica  O(M * N)    N <= 200, M <= 200
+# - cubica      O(M^2 * N)  N <= 20, M <= 20
+
+def test_case(n_u, n_v, m):
+    print(f"Evaluating test case: N_U = {n_u}, N_V = {n_u}, M = {m}...  \t", end="")
+
+    res = 0
+
+    N = n_u
+    M = n_v
+
+    # edges density
+    edges = m
+
+    # create random graph
+    G = nx.bipartite.gnmk_random_graph(N, M, edges, seed=0xC0FFEE)
+    # U is 0..N-1, V is N..N+M-1
+    U,V = range(0,N), range(N,N+M)
+
+    try:
+        with ta.run_algorithm(ta.submission.source, time_limit=0.2) as p:
+            # build the graphs
+            n1 = len(U)
+            n2 = len(V)
+            mG = edges
+
+            u = [ x for x, _ in sorted(G.edges) ]
+            v = [ y for _, y in sorted(G.edges) ]
+
+            # initialize the H graph
+            H = nx.DiGraph()
+            H_edges = -1
+            flow_start = None
+            flow_end   = None
+
+            def size_of_H(nH,mH):
+                nonlocal H, H_edges
+                if nH < 0:
+                    raise Exception('negative number of nodes')
+                if mH < 0:
+                    raise Exception('negative number of edges')
+                H.add_nodes_from(range(0,nH))
+                H_edges = mH
+
+            def add_edge(u,v,w):
+                nonlocal H, H_edges
+                if len(H.edges) == H_edges:
+                    raise Exception('too many edges added with add_edge')
+                if not H.has_node(u):
+                    raise Exception('invalid node u={}'.format(u))
+                if not H.has_node(v):
+                    raise Exception('invalid node v={}'.format(v))
+                H.add_edge(u,v, capacity=w)
+
+            def set_s(s):
+                nonlocal flow_start
+                flow_start = s
+
+            def set_t(t):
+                nonlocal flow_end
+                flow_end = t
+
+            # the solution build H
+            p.procedures.prepare_H_from_G(n1, n2, mG, u, v,
+                    callbacks=[
+                        size_of_H,
+                        add_edge,
+                        set_s,
+                        set_t
+                        ])
+
+            # test that the information provided for H are consistent
+            if flow_start is None:
+                raise Exception('flow start not set (use set_s)')
+            if flow_end is None:
+                raise Exception('flow end not set (use set_t)')
+            if len(H.edges) != H_edges:
+                raise Exception('inconsistent number of edges')
+
+            max_flow, flow = nx.algorithms.flow.maximum_flow(
+                    H, flow_start, flow_end)
+
+            matching = []
+            cover_on_U = set()
+            cover_on_V = set()
+
+            def put_in_matching(u,v):
+                nonlocal matching
+                matching.append([u,v])
+
+            def put_in_node_cover_on_side_U(u):
+                nonlocal cover_on_U
+                cover_on_U.add(u)
+
+            def put_in_node_cover_on_side_V(v):
+                nonlocal cover_on_V
+                cover_on_V.add(v)
+
+            def flow_val(u,v):
+                nonlocal flow
+                return flow[u][v]
+
+            p.procedures.max_card_matching_and_min_node_cover_in_G(n1,n2,mG,u,v,
+                    callbacks=[
+                        put_in_matching,
+                        put_in_node_cover_on_side_U,
+                        put_in_node_cover_on_side_V,
+                        flow_val
+                        ])
+
+            print("Evaluating the matching... ")
+
+            # check the matching
+            if max_flow != len(matching):
+                raise Exception('matching is not massimal')
+
+            from_set = set()
+            to_set = set()
+            for u, v in matching:
+                # is a valid edge
+                if not G.has_edge(u,v):
+                    raise Exception('({},{}) is not an edge'.format(v,u))
+
+                # single matching for every node
+                if u in from_set:
+                    raise Exception('multiple matching for u={}'.format(u))
+                from_set.add(u)
+                if v in to_set:
+                    raise Exception('multiple matching for v={}'.format(v))
+                to_set.add(v)
+
+            # The matching is correct
+
+            print("[CORRECT]")
+            res = 1
+            print("Evaluating the node cover... ")
+
+            # check the node cover
+            if max_flow != (len(cover_on_U) + len(cover_on_V)):
+                raise Exception('wrong size of cover, {} vs {}'.format(
+                    max_flow,(len(cover_on_U) + len(cover_on_V))))
+
+            for (x,y) in G.edges:
+                if not x in cover_on_U and not y in cover_on_V:
+                    raise Exception('edges ({},{}) is not covered'.format(x,y))
+
+            # The node cover is correct
+
+            print("[CORRECT]")
+            res = 2
+
+            # compute the minimum cut
+
+            min_cut, partition = nx.algorithms.flow.minimum_cut(
+                    H, flow_start, flow_end)
+
+            reachable, non_reachable = partition
+
+            cutset = set()
+            for u, nbrs in ((n, H[n]) for n in reachable):
+                cutset.update((u, v) for v in nbrs if v in non_reachable)
+
+            cutset = [ (x,y) for x,y in cutset ]
+
+            iter_cut_left = 0
+            iter_cut_right = 0
+            cover_on_U = set()
+            cover_on_V = set()
+
+            def put_in_node_cover_on_side_U(u):
+                nonlocal cover_on_U
+                cover_on_U.add(u)
+
+            def put_in_node_cover_on_side_V(v):
+                nonlocal cover_on_V
+                cover_on_V.add(v)
+
+            def min_cut_value():
+                nonlocal min_cut
+                return min_cut
+
+            def next_in_min_cut_left():
+                nonlocal min_cut, iter_cut_left, cutset
+                if iter_cut_left >= min_cut:
+                    raise Exception('cut left has only {} nodes'.format(n1))
+                iter_cut_left+=1
+                return cutset[iter_cut_left-1][0]
+
+            def next_in_min_cut_right():
+                nonlocal min_cut, iter_cut_right, cutset
+                if iter_cut_right >= min_cut:
+                    raise Exception('cut right has only {} nodes'.format(n1))
+                iter_cut_right+=1
+                return cutset[iter_cut_right-1][1]
+
+            p.procedures.min_node_cover_from_min_cut(n1,n2,
+                    callbacks=[
+                        put_in_node_cover_on_side_U,
+                        put_in_node_cover_on_side_V,
+                        min_cut_value,
+                        next_in_min_cut_left,
+                        next_in_min_cut_right
+                        ])
+
+            print("Evaluating the node cover... ")
+            # check the node cover
+            if max_flow != (len(cover_on_U) + len(cover_on_V)):
+                raise Exception('wrong size of cover, {} vs {}'.format(
+                    max_flow,(len(cover_on_U) + len(cover_on_V))))
+
+            for (x,y) in G.edges:
+                if not x in cover_on_U and not y in cover_on_V:
+                    raise Exception('edges ({},{}) is not covered'.format(x,y))
+
+            # The node cover is correct
+
+            print("[CORRECT]")
+            res = 3
+
+    except Exception as e:
+        print(f" error: {e}")
+        print("[WRONG]")
+
+    if res == 3:
+        print("[PASSED]")
+    return res
+
+
+def main():
+    for n in (10, 15, 20):
+        for m in (10, 15, 20):
+            if test_case(n, m)<3:
+                ta.goals["min_node_cover_from_min_cut"] = False
+            if test_case(n, m)<2:
+                ta.goals["min_node_cover"] = False
+            if test_case(n, m)<1:
+                ta.goals["max_card_matching"] = False
+    ta.goals.setdefault("max_card_matching", True)
+    ta.goals.setdefault("min_node_cover", True)
+    ta.goals.setdefault("min_node_cover_from_min_cut", True)
+
+    for n in (100, 150, 200):
+        for m in (100, 150, 200):
+            if test_case(n, m)<3:
+                ta.goals["min_node_cover_from_min_cut"] = False
+            if test_case(n, m)<2:
+                ta.goals["min_node_cover"] = False
+            if test_case(n, m)<1:
+                ta.goals["max_card_matching"] = False
+    ta.goals.setdefault("max_card_matching", True)
+    ta.goals.setdefault("min_node_cover", True)
+    ta.goals.setdefault("min_node_cover_from_min_cut", True)
+
+    for n in (1000, 1500, 2000):
+        for m in (1000, 1500, 2000):
+            if test_case(n, m)<3:
+                ta.goals["min_node_cover_from_min_cut"] = False
+            if test_case(n, m)<2:
+                ta.goals["min_node_cover"] = False
+            if test_case(n, m)<1:
+                ta.goals["max_card_matching"] = False
+
+    ta.goals.setdefault("max_card_matching", True)
+    ta.goals.setdefault("min_node_cover", True)
+    ta.goals.setdefault("min_node_cover_from_min_cut", True)
+
+    print(ta.goals)
+
+if __name__ == "__main__":
+    main()
