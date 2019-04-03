cae132b013b95b61581a564f183fa47d0d02f8e5
maxrossi91 <maxrossi91@gmail.com>
2019-04-03 17:28:33 +0200

    Lift time limit

--- evaluator.py
+++ evaluator.py
@@ -5,10 +5,10 @@ from networkx.algorithms import bipartite
 DEBUG = False
 
 # Goals:
-# time limit = 0.05s
+# time limit = 0.15s
 # - max_card_matching
 # - min_node_cover
-# - min_node_cover_from_min_cut   
+# - min_node_cover_from_min_cut
 
 def test_case(n_u, n_v, m):
     print(f"\nEvaluating test case: N_U = {n_u}, N_V = {n_u}, M = {m}...  ")#)\t", end="")
@@ -27,7 +27,7 @@ def test_case(n_u, n_v, m):
     U,V = range(0,N), range(N,N+M)
 
     try:
-        with ta.run_algorithm(ta.submission.source, time_limit=0.05) as p:
+        with ta.run_algorithm(ta.submission.source, time_limit=0.15) as p:
             # build the graphs
             n1 = len(U)
             n2 = len(V)
